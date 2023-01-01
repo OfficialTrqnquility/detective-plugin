a0d7fb8d874d661b279682a8cf53e066a7ff3c0f
OfficialTrqnquility <trqnquility@gmail.com>
2023-01-01 10:02:39 -0800

    Fix: Add more data to my movement packet.

--- src/main/kotlin/me/trqnquility/detective/handler/MovementListener.kt
+++ src/main/kotlin/me/trqnquility/detective/handler/MovementListener.kt
@@ -3,14 +3,34 @@ package me.trqnquility.detective.handler
 import me.trqnquility.detective.packet.PacketPlayerMove
 import me.trqnquility.detective.repository.MovementHistory
 import me.trqnquility.detective.util.toVector3F
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.entity.Entity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.potion.PotionEffectType
 
-class MovementListener(private val movementHistory: MovementHistory): Listener {
+class MovementListener(private val movementHistory: MovementHistory) : Listener {
 
     @EventHandler
     fun onMove(event: PlayerMoveEvent) {
-        movementHistory.add(event.player.uniqueId, PacketPlayerMove(event.player.location.toVector3F(), event.player.location.yaw, event.player.location.pitch, event.player.velocity.toVector3F()))
+        movementHistory.add(
+            event.player.uniqueId,
+            PacketPlayerMove(
+                event.from.toVector3F(),
+                event.to.toVector3F(),
+                event.from.yaw,
+                event.to.yaw,
+                event.from.pitch,
+                event.to.pitch,
+                event.player.velocity.toVector3F(),
+                (event.player as Entity).isOnGround,
+                event.player.isSneaking,
+                event.player.activePotionEffects.find { it.type == PotionEffectType.SPEED }?.amplifier ?: 0,
+                (event.player.isInWater || event.player.isInLava),
+                event.player.isSwimming,
+                event.player.equipment.boots?.getEnchantmentLevel(Enchantment.DEPTH_STRIDER) ?: 0,
+            )
+        )
     }
 }
--- src/main/kotlin/me/trqnquility/detective/packet/PacketPlayerMove.kt
+++ src/main/kotlin/me/trqnquility/detective/packet/PacketPlayerMove.kt
@@ -2,4 +2,18 @@ package me.trqnquility.detective.packet
 
 import me.trqnquility.detective.util.Vector3F
 
-data class PacketPlayerMove(val location: Vector3F, val yaw: Float, val pitch: Float, val velocity: Vector3F)
+data class PacketPlayerMove(
+    val fromLocation: Vector3F,
+    val toLocation: Vector3F,
+    val fromYaw: Float,
+    val toYaw: Float,
+    val fromPitch: Float,
+    val toPitch: Float,
+    val velocity: Vector3F,
+    val onGround: Boolean,
+    val shifting: Boolean,
+    val speed: Int,
+    val inLiquid: Boolean,
+    val swimming: Boolean,
+    val depthStrider: Int,
+)
